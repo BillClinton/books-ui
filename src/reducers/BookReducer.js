c744fc7065ed9f8716e7231f40ea7b8ff7404658
BillClinton <bclinton@appunto.net>
2020-04-15 21:54:58 -0400

    Add READ method and fix syntax issues

--- src/reducers/BookReducer.js
+++ src/reducers/BookReducer.js
@@ -1,6 +1,7 @@
 import {
   CREATE_BOOK,
   READ_BOOKS,
+  READ_BOOK,
   UPDATE_BOOK,
   DESTROY_BOOK,
 } from '../actions/types';
@@ -10,7 +11,7 @@ export default (state = {}, action) => {
     case CREATE_BOOK: {
       return {
         ...state,
-        books: [...state.books, action.payload.book],
+        books: [...state.books, action.payload],
       };
     }
 
@@ -18,10 +19,14 @@ export default (state = {}, action) => {
       return { ...state, books: action.payload };
     }
 
+    case READ_BOOK: {
+      return { ...state, book: action.payload };
+    }
+
     case UPDATE_BOOK: {
       const books = state.books;
       const book = action.payload;
-      const index = books.findIndex((item) => item._id === book._id);
+      const index = books.findIndex((item) => item.id === book.id);
 
       if (~index) {
         books[index] = book;
@@ -31,7 +36,9 @@ export default (state = {}, action) => {
     }
 
     case DESTROY_BOOK: {
-      const books = state.books.filter((apt) => apt._id !== action.payload);
+      const books = state.books.filter(
+        (bk) => bk.id !== parseInt(action.payload, 10)
+      );
       return { ...state, books: books };
     }
 
