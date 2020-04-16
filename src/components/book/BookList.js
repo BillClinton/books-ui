1f0a6dd4934370e03c89bdc61936fb1702223f19
BillClinton <bclinton@appunto.net>
2020-04-15 21:56:57 -0400

    Add action links to book list

--- src/components/book/BookList.js
+++ src/components/book/BookList.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { BookStore } from '../../contexts/BookStore';
 import BookDetails from './BookDetails';
 
@@ -9,9 +10,21 @@ const BookList = () => {
   console.log(books);
   return books && books.length ? (
     <div className="book-list">
+      <h1>
+        Book list
+        <Link to="/books/new">New</Link>
+      </h1>
       <ul>
         {books.map((book) => {
-          return <BookDetails book={book} key={book.id} />;
+          return (
+            <>
+              <BookDetails book={book} key={book.id} />
+              <div>
+                <Link to={`/books/edit/${book.id}`}>edit</Link>
+                <Link to={`/books/delete/${book.id}`}>delete</Link>
+              </div>
+            </>
+          );
         })}
       </ul>
     </div>
