86c3c9934c0359616542e1269b47a83757161698
BillClinton <bclinton@appunto.net>
2020-05-05 15:40:33 -0400

    Add chakra ui and prompt modal before route change

--- src/components/book/BookList.js
+++ src/components/book/BookList.js
@@ -1,25 +1,57 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { BookStore } from '../../contexts/BookStore';
 import BookDetails from './BookDetails';
+import ConfirmDeleteModal from './ConfirmDeleteModal';
 import styles from './styles/BookList.module.scss';
+import { Button, Icon } from '@chakra-ui/core';
 
 const BookList = () => {
   const { store } = useContext(BookStore);
+  const history = useHistory();
+  const [condemnedBook, setCondemnedBook] = useState(null);
   const books = store.data;
 
+  const editBook = (book) => {
+    history.push(`/books/edit/${book.id}`);
+  };
+
+  const addBook = (book) => {
+    history.push(`/books/new`);
+  };
+
   return books && books.length ? (
-    <div className={styles.books}>
-      <div className={styles.header}>
-        <h1>Book list</h1>
-        <Link to="/books/new">New</Link>
+    <>
+      <ConfirmDeleteModal
+        book={condemnedBook}
+        clearCondemned={() => setCondemnedBook(null)}
+      />
+      <div className={styles.books}>
+        <div className={styles.header}>
+          <h1>Book list</h1>
+          <Button
+            aria-label="Add a book"
+            m="1"
+            size="sm"
+            variantColor="green"
+            onClick={() => addBook()}
+          >
+            Add a book
+          </Button>
+        </div>
+        <ul className={styles.booklist}>
+          {books.map((book) => {
+            return (
+              <BookDetails
+                book={book}
+                key={book.id}
+                onDelete={setCondemnedBook}
+              />
+            );
+          })}
+        </ul>
       </div>
-      <ul className={styles.booklist}>
-        {books.map((book) => {
-          return <BookDetails book={book} key={book.id} />;
-        })}
-      </ul>
-    </div>
+    </>
   ) : (
     <div className="empty">No books found</div>
   );
