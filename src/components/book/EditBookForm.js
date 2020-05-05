2791a37671f4da2d420429ccad9603dd555fa071
BillClinton <bclinton@appunto.net>
2020-05-05 16:08:01 -0400

    Add chakra ui form elements

--- src/components/book/EditBookForm.js
+++ src/components/book/EditBookForm.js
@@ -4,6 +4,14 @@ import { BookStore } from '../../contexts/BookStore';
 import TextInput from '../form/TextInput';
 import AuthorSelectInput from '../form/AuthorSelectInput';
 import history from '../../history';
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Flex,
+} from '@chakra-ui/core';
 
 const EditBookForm = ({ book, authors }) => {
   const form = useForm({ defaultValues: book });
@@ -18,12 +26,18 @@ const EditBookForm = ({ book, authors }) => {
   return book && authors ? (
     <form className="form" onSubmit={form.handleSubmit(onSubmit)}>
       <h1>Edit book</h1>
-      <TextInput
-        form={form}
-        fieldName="name"
-        label="Name"
-        validations={{ required: true }}
-      />
+
+      <FormControl isInvalid={form.errors.name}>
+        <FormLabel htmlFor="name">Name</FormLabel>
+        <Input
+          name="name"
+          placeholder="name"
+          ref={form.register({ required: true })}
+        />
+        <FormErrorMessage>
+          {form.errors.name && form.errors.name.message}
+        </FormErrorMessage>
+      </FormControl>
 
       <AuthorSelectInput
         form={form}
@@ -33,12 +47,14 @@ const EditBookForm = ({ book, authors }) => {
         values={book.authors}
       />
 
-      <div className="buttons">
-        <button className="submit">Submit</button>
-        <button className="cancel" onClick={onCancel}>
+      <Flex justify="center" p={2} w="100%" align="center">
+        <Button className="cancel" onClick={onCancel} m="1">
           Cancel
-        </button>
-      </div>
+        </Button>
+        <Button className="submit" m="1" type="submit">
+          Save
+        </Button>
+      </Flex>
     </form>
   ) : (
     <div>Loading....</div>
