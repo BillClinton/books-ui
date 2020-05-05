9b381f474e94aef2ebe49613b2361c5411f6e0cd
BillClinton <bclinton@appunto.net>
2020-05-05 15:45:07 -0400

    Add chakra ui and prompt modal before route change

--- src/components/author/AuthorList.js
+++ src/components/author/AuthorList.js
@@ -1,18 +1,33 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { AuthorStore } from '../../contexts/AuthorStore';
 import AuthorDetails from './AuthorDetails';
 import styles from './styles/AuthorList.module.scss';
+import { Button } from '@chakra-ui/core';
 
 const AuthorList = () => {
   const { store } = useContext(AuthorStore);
+  const history = useHistory();
+  const [condemnedBook, setCondemnedBook] = useState(null);
   const authors = store.data;
+
+  const addAuthor = (author) => {
+    history.push(`/author/new`);
+  };
 
   return authors && authors.length ? (
     <div className={styles.authors}>
       <div className={styles.header}>
         <h1>Author list</h1>
-        <Link to="/authors/new">New</Link>
+        <Button
+          aria-label="Add an author"
+          m="1"
+          size="sm"
+          variantColor="green"
+          onClick={() => addAuthor()}
+        >
+          Add an author
+        </Button>
       </div>
       <ul className={styles.authorlist}>
         {authors.map((author) => {
