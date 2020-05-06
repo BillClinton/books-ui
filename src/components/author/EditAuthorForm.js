597f46907ee5128272c049dd2ca7e792046d9c64
BillClinton <bclinton@appunto.net>
2020-05-05 23:56:25 -0400

    Add chakra-ui styling to edit form

--- src/components/author/EditAuthorForm.js
+++ src/components/author/EditAuthorForm.js
@@ -1,8 +1,15 @@
 import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { AuthorStore } from '../../contexts/AuthorStore';
-import TextInput from '../form/TextInput';
 import history from '../../history';
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Flex,
+} from '@chakra-ui/core';
 
 const EditAuthorForm = ({ author }) => {
   const form = useForm({ defaultValues: author });
@@ -17,19 +24,26 @@ const EditAuthorForm = ({ author }) => {
   return author ? (
     <form className="form" onSubmit={form.handleSubmit(onSubmit)}>
       <h1>Edit author</h1>
-      <TextInput
-        form={form}
-        fieldName="name"
-        label="Name"
-        validations={{ required: true }}
-      />
+      <FormControl isInvalid={form.errors.name}>
+        <FormLabel htmlFor="name">Name</FormLabel>
+        <Input
+          name="name"
+          placeholder="name"
+          ref={form.register({ required: true })}
+        />
+        <FormErrorMessage>
+          {form.errors.name && form.errors.name.message}
+        </FormErrorMessage>
+      </FormControl>
 
-      <div className="buttons">
-        <button className="submit">Submit</button>
-        <button className="cancel" onClick={onCancel}>
+      <Flex justify="center" p={2} w="100%" align="center">
+        <Button className="cancel" onClick={onCancel} m="1">
           Cancel
-        </button>
-      </div>
+        </Button>
+        <Button className="submit" m="1" type="submit">
+          Save
+        </Button>
+      </Flex>
     </form>
   ) : (
     <div>Loading....</div>
