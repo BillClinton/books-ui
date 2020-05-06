ae23acd630508aae4c0e174c07925ee4224bd4d0
BillClinton <bclinton@appunto.net>
2020-05-05 23:55:40 -0400

    Fix new author form, ads chakra styling

--- src/components/author/NewAuthorForm.js
+++ src/components/author/NewAuthorForm.js
@@ -1,8 +1,15 @@
 import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { AuthorStore } from '../../contexts/AuthorStore';
-import TextInput from '../form/TextInput';
 import history from '../../history';
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Flex,
+} from '@chakra-ui/core';
 
 const NewAuthorForm = () => {
   const form = useForm();
@@ -15,25 +22,28 @@ const NewAuthorForm = () => {
   };
 
   return (
-    <form
-      className="form"
-      onSubmit={form.handleSubmit(onSubmit)}
-      autoComplete="on"
-    >
+    <form className="form" onSubmit={form.handleSubmit(onSubmit)}>
       <h1>Add a new author</h1>
-      <TextInput
-        form={form}
-        fieldName="name"
-        label="Name"
-        validations={{ required: true }}
-      />
+      <FormControl isInvalid={form.errors.name}>
+        <FormLabel htmlFor="name">Name</FormLabel>
+        <Input
+          name="name"
+          placeholder="name"
+          ref={form.register({ required: true })}
+        />
+        <FormErrorMessage>
+          {form.errors.name && form.errors.name.message}
+        </FormErrorMessage>
+      </FormControl>
 
-      <div className="buttons">
-        <button className="submit">Submit</button>
-        <button className="cancel" onClick={onCancel}>
+      <Flex justify="center" p={2} w="100%" align="center">
+        <Button className="cancel" onClick={onCancel} m="1">
           Cancel
-        </button>
-      </div>
+        </Button>
+        <Button className="submit" m="1" type="submit">
+          Save
+        </Button>
+      </Flex>
     </form>
   );
 };
