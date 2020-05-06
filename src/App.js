e7ee83e8d2be601aeb715314103529faf6ff829b
BillClinton <bclinton@appunto.net>
2020-05-06 15:42:41 -0400

    Add Auth context provider to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import history from './history';
 import Nav from './components/layout/Nav';
 import BookHome from './components/book/BookHome';
 import AuthorHome from './components/author/AuthorHome';
+import AuthContextProvider from './contexts/AuthContext';
 import LoginForm from './components/user/LoginForm';
 import NoRouteFound from './components/NoRouteFound';
 import { ThemeProvider, CSSReset, Box } from '@chakra-ui/core';
@@ -12,34 +13,36 @@ import './App.css';
 
 function App() {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CSSReset />
-      <Router history={history}>
-        <Nav />
-        <Box
-          mx="auto"
-          width={[
-            '100%', // base
-            '90%', // 480px upwards
-            '90%', // 768px upwards
-            '70%', // 992px upwards
-          ]}
-        >
-          <Switch>
-            <Route path="/books">
-              <BookHome />
-            </Route>
-            <Route path="/authors">
-              <AuthorHome />
-            </Route>
-            <Route path="/login">
-              <LoginForm />
-            </Route>
-            <Route path="*" component={NoRouteFound} />
-          </Switch>
-        </Box>
-      </Router>
-    </ThemeProvider>
+    <AuthContextProvider>
+      <ThemeProvider theme={customTheme}>
+        <CSSReset />
+        <Router history={history}>
+          <Nav />
+          <Box
+            mx="auto"
+            width={[
+              '100%', // base
+              '90%', // 480px upwards
+              '90%', // 768px upwards
+              '70%', // 992px upwards
+            ]}
+          >
+            <Switch>
+              <Route path="/books">
+                <BookHome />
+              </Route>
+              <Route path="/authors">
+                <AuthorHome />
+              </Route>
+              <Route path="/login">
+                <LoginForm />
+              </Route>
+              <Route path="*" component={NoRouteFound} />
+            </Switch>
+          </Box>
+        </Router>
+      </ThemeProvider>
+    </AuthContextProvider>
   );
 }
 
