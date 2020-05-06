4a88c3077ddec1045c0f97eed7e8b95c80aecfc4
BillClinton <bclinton@appunto.net>
2020-05-06 00:00:06 -0400

    Fix author select on new book form

--- src/components/book/NewBookForm.js
+++ src/components/book/NewBookForm.js
@@ -1,39 +1,63 @@
 import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { BookStore } from '../../contexts/BookStore';
-import TextInput from '../form/TextInput';
+import { AuthorStore } from '../../contexts/AuthorStore';
+import AuthorSelectInput from '../form/AuthorSelectInput';
 import history from '../../history';
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Flex,
+} from '@chakra-ui/core';
 
 const NewBookForm = () => {
   const form = useForm();
-  const { store } = useContext(BookStore);
+  const { store: bookStore } = useContext(BookStore);
+  const { store: authorStore } = useContext(AuthorStore);
+
+  const authors = authorStore.data;
 
   const onCancel = () => history.push('/books');
   const onSubmit = (data, e) => {
     e.preventDefault();
-    store.create(data);
+    bookStore.create(data);
   };
 
   return (
-    <form
-      className="form"
-      onSubmit={form.handleSubmit(onSubmit)}
-      autoComplete="on"
-    >
+    <form className="form" onSubmit={form.handleSubmit(onSubmit)}>
       <h1>Add a new book</h1>
-      <TextInput
+
+      <FormControl isInvalid={form.errors.name}>
+        <FormLabel htmlFor="name">Name</FormLabel>
+        <Input
+          name="name"
+          placeholder="name"
+          ref={form.register({ required: true })}
+        />
+        <FormErrorMessage>
+          {form.errors.name && form.errors.name.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <AuthorSelectInput
         form={form}
-        fieldName="name"
-        label="Name"
-        validations={{ required: true }}
+        fieldName="authors"
+        label="Author(s)"
+        options={authors}
+        // values={book.authors}
       />
 
-      <div className="buttons">
-        <button className="submit">Submit</button>
-        <button className="cancel" onClick={onCancel}>
+      <Flex justify="center" p={2} w="100%" align="center">
+        <Button className="cancel" onClick={onCancel} m="1">
           Cancel
-        </button>
-      </div>
+        </Button>
+        <Button className="submit" m="1" type="submit">
+          Save
+        </Button>
+      </Flex>
     </form>
   );
 };
