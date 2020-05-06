f1774dc957965a2a28c070623317124de9b4a3e9
BillClinton <bclinton@appunto.net>
2020-05-05 23:30:52 -0400

    Add 404 page and add to author router/switch

---
+++ src/components/NoRouteFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/core';
+
+const NoRouteFound = () => {
+  return (
+    <Flex align="center" justify="center" mt="100px">
+      The requested page was not found
+    </Flex>
+  );
+};
+
+export default NoRouteFound;
--- src/components/author/AuthorHome.js
+++ src/components/author/AuthorHome.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-
+import { Route, Switch } from 'react-router-dom';
 import AuthorStoreProvider from '../../contexts/AuthorStore';
 import AuthorList from './AuthorList';
 import NewAuthorForm from './NewAuthorForm';
 import EditAuthor from './EditAuthor';
-import DeleteAuthor from './DeleteAuthor';
+import NoRouteFound from '../NoRouteFound';
 
 function AuthorHome() {
   return (
     <>
       <AuthorStoreProvider>
-        <Route exact path="/authors" component={AuthorList} />
-        <Route exact path="/authors/new" component={NewAuthorForm} />
-        <Route exact path="/authors/edit/:id" component={EditAuthor} />
-        <Route exact path="/authors/delete/:id" component={DeleteAuthor} />
+        <Switch>
+          <Route exact path="/authors" component={AuthorList} />
+          <Route exact path="/authors/new" component={NewAuthorForm} />
+          <Route exact path="/authors/edit/:id" component={EditAuthor} />
+          <Route path="*" component={NoRouteFound} />
+        </Switch>
       </AuthorStoreProvider>
     </>
   );
