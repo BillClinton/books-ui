19a5f3813dcc981f6d848bc4e4dad164b8a20c19
BillClinton <bclinton@appunto.net>
2020-05-05 23:13:29 -0400

    Add chakra-ui styling

--- src/components/author/AuthorDetails.js
+++ src/components/author/AuthorDetails.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import styles from './styles/AuthorDetails.module.scss';
+import { IconButton } from '@chakra-ui/core';
 
-const AuthorDetails = ({ author }) => {
+const AuthorDetails = ({ author, onDelete }) => {
+  const history = useHistory();
+
+  const editAuthor = (author) => history.push(`/authors/edit/${author.id}`);
+
   return (
     <li className={styles.row}>
       <div className={styles.item}>
         <div>{author.name}</div>
       </div>
       <div className={styles.item}>
-        <Link to={`/authors/edit/${author.id}`}>edit</Link>
-        <Link to={`/authors/delete/${author.id}`}>delete</Link>
+        <IconButton
+          aria-label="Edit book"
+          icon="edit"
+          m="1"
+          size="sm"
+          onClick={() => editAuthor(author)}
+        />
+        <IconButton
+          aria-label="Delete book"
+          icon="delete"
+          m="1"
+          size="sm"
+          onClick={() => onDelete(author)}
+        />
       </div>
     </li>
   );
