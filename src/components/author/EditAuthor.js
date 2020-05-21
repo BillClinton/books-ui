e09eb38414e70f3cacb30392ab3a1af84d426754
BillClinton <bclinton@appunto.net>
2020-05-21 04:08:35 -0400

    Add error toasts to edit author form

--- src/components/author/EditAuthor.js
+++ src/components/author/EditAuthor.js
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect } from 'react';
+import { useToast } from '@chakra-ui/core';
 import { AuthorStore } from '../../contexts/AuthorStore';
 import EditAuthorForm from './EditAuthorForm';
 import LoadingSpinner from '../layout/LoadingSpinner';
 
 const EditAuthor = ({ match }) => {
   const { store } = useContext(AuthorStore);
+  const toast = useToast();
   const id = parseInt(match.params.id, 10);
   let author = store.item;
 
@@ -12,10 +14,32 @@ const EditAuthor = ({ match }) => {
     store.readItem(id);
   }, [id]);
 
+  useEffect(() => {
+    if (store.failure) {
+      let title = 'An error occurred.';
+      if (store.matchState({ item: 'failure' })) {
+        title = 'Error attempting to load author.';
+        store.send({ to: 'item', type: 'reset' });
+      }
+      if (store.matchState({ patch: 'failure' })) {
+        title = 'Error attempting to update author.';
+        store.send({ to: 'patch', type: 'reset' });
+      }
+
+      toast({
+        title,
+        description: store.message,
+        status: 'warning',
+        duration: 4000,
+        isClosable: true,
+      });
+    }
+  }, [store.failure, store, toast]);
+
   return (
     <>
-      {store.matchItemState('itemPending') ? <LoadingSpinner /> : null}
-      {store.matchItemState('success') ? (
+      {store.matchState({ item: 'pending' }) ? <LoadingSpinner /> : null}
+      {store.matchState({ item: 'success' }) ? (
         <EditAuthorForm author={author} />
       ) : null}
     </>
